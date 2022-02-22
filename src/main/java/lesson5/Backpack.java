fbc61401d6fed35ba3a7c7b1c1e6b572a3787c8c
alexf <rassylka82@inbox.ru>
2022-02-22 19:35:56 +0300

    lesson5 - ДЗ

---
+++ src/main/java/lesson5/Backpack.java
@@ -0,0 +1,20 @@
+package lesson5;
+
+public class Backpack {
+
+    private String name;
+    private int capacity;
+
+    public Backpack(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
